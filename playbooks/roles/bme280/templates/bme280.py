723388b9f793172461bfa7079ec36a9df26d05e5
RW21 <watanabe@nablas.com>
2022-06-26 17:30:48 +0900

    Update

--- playbooks/roles/bme280/templates/bme280.py
+++ playbooks/roles/bme280/templates/bme280.py
@@ -10,7 +10,7 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 # You can generate an API token from the "API Tokens Tab" in the UI
-token = os.getenv('INFLUX_KEY')
+token = "{{ influx_api_token }}"
 org = "homelab"
 bucket = "home"
 
@@ -22,9 +22,8 @@ temperature = bme280.temperature
 pressure = bme280.pressure
 humidity = bme280.humidity
 
-with InfluxDBClient(url="http://192.168.193.179:32481", token=token, org=org) as client:
+with InfluxDBClient(url="http://0.0.0.0:2029", token=token, org=org) as client:
     data = 'BME280,sensor=bme280 celsius={:05.2f},hPa={:05.2f},humidity={:02.2f}'.format(temperature, pressure, humidity)
 
     write_api = client.write_api(write_options=SYNCHRONOUS)
     write_api.write(bucket, org, data)
-
